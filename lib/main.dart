6c5ab8f7cea8fa0ad8faf9a58aaac2a90e72c0d2
roushjac <roush.jake@gmail.com>
2019-04-20 14:08:20 -0400

    Add a picture of food, add pic as an asset, add a button that doesn't do anything (yet)

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,32 @@
 import 'package:flutter/material.dart';
 
-main() {
+void main() {
   // main() is a special function that is run by Flutter automatically on "flutter run"
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  build(context) {
+  @override // MyApp's build() method overrides the build() method already present in StatelessWidget
+  Widget build(BuildContext context) {
+    // Give the build() method what type it should return - here its a widget
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('EasyList'),
         ),
+        body: Column(children: <Widget>[
+          RaisedButton(
+            child: Text('Add something'),
+            onPressed: () {}
+          ),
+          Card(
+              child: Column(
+            children: <Widget>[
+              Image.asset('assets/food_pic.jpg'),
+              Text('Fooooooood')
+            ],
+          )),
+        ]),
       ),
     );
   }
